4877ec27839a8d5684c784171032610a284cbcf3
Randy <yrkrandy0207@163.com>
2023-05-22 21:06:52 +0800

    学生更新实习单位指导意见及视图更新

--- ims-app/src/main/java/com/kc/plat/common/Constant.java
+++
@@ -1,8 +0,0 @@
-package com.kc.plat.common;
-
-public class Constant {
-    //登录失效
-    public final static int LOGIN_VALID = 100001;
-    //异常信息
-    public final static int LOGIN_VALID1 = 100002;
-}
