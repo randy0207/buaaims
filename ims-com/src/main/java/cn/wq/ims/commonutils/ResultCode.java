09ba4c861c7325510cb9632733a7c905e278b26c
randy0207 <yrkrandy0207@163.com>
2023-06-01 14:08:51 +0800

    update ims

---
+++ ims-com/src/main/java/cn/wq/ims/commonutils/ResultCode.java
@@ -0,0 +1,11 @@
+package cn.wq.ims.commonutils;
+
+/**
+ * @Author:WQ
+ * @Date:2022-04-18-14:59
+ * @Description:
+ */
+public interface ResultCode {
+    public static final Integer SUCCESS = 20000;
+    public static final Integer ERROR =20001;
+}
